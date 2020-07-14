93d0937d33a49704a0ba74cc769691ce66617589
Narazaka <info@narazaka.net>
2020-07-14 16:45:30 +0900

    bundle gem remain_timer -t minitest

---
+++ lib/remain_timer.rb
@@ -0,0 +1,6 @@
+require "remain_timer/version"
+
+module RemainTimer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/remain_timer/version.rb
@@ -0,0 +1,3 @@
+module RemainTimer
+  VERSION = "0.1.0"
+end

b83d2d98c1164bf00f77757230094f0a9e2c88db
Narazaka <info@narazaka.net>
2020-10-02 18:14:42 +0900

    duration

---
+++ lib/duration_format.rb
@@ -0,0 +1,40 @@
+class DurationFormat
+  SECONDS_PER_DAY = 86400
+  SECONDS_PER_HOUR = 3600
+  SECONDS_PER_MINUTE = 60
+  PARTS = %i[day hour min sec].freeze
+  PARTS_IN_SECONDS = { sec: 1, min: SECONDS_PER_MINUTE, hour: SECONDS_PER_HOUR, day: SECONDS_PER_DAY }.freeze
+
+  def self.build_parts(value)
+    parts = {}
+    remainder = value.to_f
+
+    PARTS.each do |part|
+      next if part == :sec
+      part_in_seconds = PARTS_IN_SECONDS[part]
+      parts[part] = remainder.div(part_in_seconds)
+      remainder = (remainder % part_in_seconds).round(9)
+    end
+
+    parts[:sec] = remainder
+
+    parts
+  end
+
+  def self.format(value)
+    parts = build_parts(value)
+    (parts[:day] > 0 ? "#{parts[:day]}day " : "") + sprintf('%02d:%02d:%02d', parts[:hour], parts[:min], parts[:sec].round)
+  end
+end
+
+class Integer
+  def duration_format
+    DurationFormat.format(self)
+  end
+end
+
+class Float
+  def duration_format
+    DurationFormat.format(self)
+  end
+end
